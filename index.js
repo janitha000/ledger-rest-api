9092da317ac0d0a11a10a3755ca4c4bc926cf644
Janitha Tennakoon <janitha000@gmail.com>
2021-04-10 14:59:10 +0800

    add swagger api

--- index.js
+++ index.js
@@ -2,11 +2,18 @@ const express = require('express');
 const logger = require('./utils/logger');
 
 const app = express();
+const leaseRouter = require('./routes/lease-routes')
+const handleErrors = require('./middlewares/errorHandleMiddleware')
 
 const PORT = process.env.PORT || 5000;
 
+const swaggerUi = require('swagger-ui-express');
+const YAML = require('yamljs');
+const swaggerDocument = YAML.load('./swagger.yaml');
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use('/leases', leaseRouter)
+app.use(handleErrors);
 
 app.listen(PORT, () => {
     logger.info("🚀 The server started on port " + PORT)
-    console.log("🚀 The server started on port " + PORT);
 });
