3f4a5b5a5bdd64ea7e19ab53407b9c57a9fece72
Janitha Tennakoon <janitha000@gmail.com>
2021-04-11 08:50:33 +0800

    Increae accesstoken expiry time

--- token-generation.js
+++ token-generation.js
@@ -4,7 +4,7 @@ dotenv.config();
 
 
 module.exports.generateToken = () => {
-    return jwt.sign({ username: "admin", userRights: ["admin"] }, process.env.TOKEN_SECRET = "SECRET", { expiresIn: '1800s' });
+    return jwt.sign({ username: "admin", userRights: ["admin"] }, process.env.TOKEN_SECRET = "SECRET", { expiresIn: '7200s' });
 }
 
 console.log(this.generateToken())
